8568d7dc200cc66e47b82acfa638421da4efaf5f
Alexandre Del Vecchio <alexandre@buyco.co>
2019-12-04 14:07:22 +0100

    Move coverage formatter script to tools directory

--- internal/utils/map.go
+++
@@ -1,9 +0,0 @@
-package utils
-
-func CopyMap(originalMap map[string]interface{}) map[string]interface{} {
-	newMap := make(map[string]interface{})
-	for key, values := range originalMap {
-		newMap[key] = values
-	}
-	return newMap
-}
